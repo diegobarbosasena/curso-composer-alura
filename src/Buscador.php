c2acfd207993e4777a0516cb536396c58dc975b3
Diego Barbosa Sena <diegosena_93@hotmail.com>
2020-02-08 23:26:26 -0300

    quarto commit

--- src/Buscador.php
+++ src/Buscador.php
@@ -8,13 +8,9 @@ use Symfony\Component\DomCrawler\Crawler;
 
 class Buscador
 {
-    /**
-     * @var ClientInterface
-     */
+
     private $httpClient;
-    /**
-     * @var Crawler
-     */
+
     private $crawler;
 
     public function __construct(ClientInterface $httpClient, Crawler $crawler)
